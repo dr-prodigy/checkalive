8fc89f8ca0893e1a2d86f678cb6cc59be6546149
dr-prodigy <dr.prodigy.github@gmail.com>
2023-07-27 17:15:31 +0200

    v.1.0

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git clone https://github.com/fabianonline/telegram.sh.git
+echo checkalive - Installation complete!
+echo Run as
+echo $ checkalive ...
+echo or schedule with crontab -e
