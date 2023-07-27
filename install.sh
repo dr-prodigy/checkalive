259eb5efd628b2a48a399b6a455379364864c84a
dr-prodigy <dr.prodigy.github@gmail.com>
2023-07-27 17:34:01 +0200

    Telegram install fix

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone https://github.com/fabianonline/telegram.sh.git
+git clone https://github.com/fabianonline/telegram.sh.git ~/.telegram-sh
 echo checkalive - Installation complete!
 echo Run as
 echo $ checkalive ...
